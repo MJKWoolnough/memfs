8f8bf053dd1d0068c3737afe11fa351c6c12c2f3
Michael Woolnough <michael.woolnough@gmail.com>
2023-06-06 20:21:00 +0100

    added stub directory methods

--- directory.go
+++ directory.go
@@ -1,3 +1,48 @@
 package memfs
 
+import (
+	"io/fs"
+	"time"
+)
+
 type directory struct{}
+
+func (d *directory) Stat() (fs.FileInfo, error) {
+	return d, nil
+}
+
+func (d *directory) Read(_ []byte) (int, error) {
+	return 0, nil
+}
+
+func (d *directory) Close() error {
+	return nil
+}
+
+func (d *directory) ReadDir(n int) ([]fs.DirEntry, error) {
+	return nil, nil
+}
+
+func (d *directory) Name() string {
+	return ""
+}
+
+func (d *directory) Size() int64 {
+	return 0
+}
+
+func (d *directory) Mode() fs.FileMode {
+	return 0
+}
+
+func (d *directory) ModTime() time.Time {
+	return time.Time{}
+}
+
+func (d *directory) IsDir() bool {
+	return true
+}
+
+func (d *directory) Sys() any {
+	return d
+}

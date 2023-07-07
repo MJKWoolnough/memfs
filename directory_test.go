65044c5eb17844069052535d13b528ebfb6c622d
Michael Woolnough <michael.woolnough@gmail.com>
2023-07-07 08:40:56 +0100

    updated dir tests

--- directory_test.go
+++ directory_test.go
@@ -46,6 +46,7 @@ func makeDirectory(dirs []*dirEnt) *directory {
 	return &directory{
 		dnode: &dnode{
 			entries: dirs,
+			mode:    fs.ModeDir | fs.ModePerm,
 		},
 	}
 }
@@ -118,63 +119,5 @@ func TestReadDir(t *testing.T) {
 		} else if !reflect.DeepEqual(asDirEntries(test), twos) {
 			t.Errorf("test %d.3: 'twos' does not equal", n+1)
 		}
-	}
-}
-
-func TestDnodeGet(t *testing.T) {
-	d := dnode{
-		entries: []*dirEnt{
-			{
-				name: "1",
-			},
-			{
-				name: "2",
-				directoryEntry: &dnode{
-					mode: fs.ModePerm,
-				},
-			},
-			{
-				name: "3",
-				directoryEntry: &dnode{
-					mode: 0o222,
-				},
-			},
-			{
-				name: "4",
-				directoryEntry: &dnode{
-					mode: 0o444,
-				},
-			},
-		},
-	}
-
-	if got, err := d.getEntry("1"); !errors.Is(err, fs.ErrPermission) {
-		t.Errorf("test 1: expecting to err %v, got %v", fs.ErrPermission, err)
-	} else if got != nil {
-		t.Errorf("test 1: expecting to get nil, got %v", got)
-	}
-
-	if got, err := d.getEntry("2"); err != nil {
-		t.Errorf("test 2: expecting to nil err, got %v", err)
-	} else if got == nil || got.name != "2" {
-		t.Errorf("test 2: expecting to get '2', got %v", got)
-	}
-
-	if got, err := d.getEntry("3"); err != nil {
-		t.Errorf("test 3: expecting to nil err, got %v", err)
-	} else if got == nil || got.name != "3" {
-		t.Errorf("test 3: expecting to get '3', got %v", got)
-	}
-
-	if got, err := d.getEntry("4"); !errors.Is(err, fs.ErrPermission) {
-		t.Errorf("test 4: expecting to err %v, got %v", fs.ErrPermission, err)
-	} else if got != nil {
-		t.Errorf("test 4: expecting to get nil, got %v", got)
-	}
-
-	if got, err := d.getEntry("5"); !errors.Is(err, fs.ErrNotExist) {
-		t.Errorf("test 5: expecting to err %v, got %v", fs.ErrNotExist, err)
-	} else if got != nil {
-		t.Errorf("test 5: expecting to get nil, got %v", got)
 	}
 }

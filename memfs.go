767e801e31d09cfbad08e63d664fc68145dd9ff1
Michael Woolnough <michael.woolnough@gmail.com>
2024-05-28 22:02:40 +0100

    rewrote path resolver

--- memfs.go
+++ memfs.go
@@ -5,6 +5,7 @@ package memfs // import "vimagination.zapto.org/memfs"
 import (
 	"errors"
 	"io/fs"
+	"path"
 	"path/filepath"
 	"strings"
 )
@@ -65,63 +66,89 @@ func (f *fsRO) getEntry(path string) (directoryEntry, error) {
 	return f.getEntryWithoutCheck(path)
 }
 
+type resolver struct {
+	fullPath, path     string
+	cutAt              int
+	redirectsRemaining uint8
+}
+
 func (f *fsRO) getEntryWithoutCheck(path string) (directoryEntry, error) {
-	if f.de.Mode()&0o444 == 0 {
-		return nil, fs.ErrPermission
+	r := resolver{
+		fullPath:           path,
+		path:               path,
+		redirectsRemaining: maxRedirects,
 	}
 
-	path = f.joinRoot(path)
-	remainingRedirects := maxRedirects
+	return r.resolve(f.de)
+}
 
-	curr := f.de
-	currPath := slash
-	path = path[1:]
+func (r *resolver) resolve(root directoryEntry) (directoryEntry, error) {
+	curr := root
 
-	for path != "" {
-		slashPos := strings.Index(path, slash)
-
-		var name string
-
-		if slashPos == -1 {
-			name = path
-			path = ""
-		} else {
-			name = path[:slashPos]
-			path = path[slashPos+1:]
-		}
-
-		if name == "" {
+	for r.path != "" {
+		if curr.Mode()&0o444 == 0 {
+			return nil, fs.ErrPermission
+		} else if name := r.splitOffNamePart(); isEmptyName(name) {
 			continue
-		}
-
-		if next, err := curr.getEntry(name); err != nil {
+		} else if next, err := curr.getEntry(name); err != nil {
 			return nil, err
 		} else if next.Mode()&fs.ModeSymlink == 0 {
 			curr = next.directoryEntry
-			currPath = filepath.Join(currPath, name)
-		} else if remainingRedirects == 0 {
-			return nil, fs.ErrInvalid
-		} else {
-			remainingRedirects--
 
-			b, err := next.bytes()
-			if err != nil {
-				return nil, err
-			}
-
-			link := filepath.Clean(string(b))
-
-			if !strings.HasPrefix(link, slash) {
-				link = filepath.Join(currPath, link)
-			}
-
-			currPath = slash
-			path = filepath.Join(link, path)
-			curr = f.de
+			continue
+		} else if err = r.handleSymlink(next); err != nil {
+			return nil, err
 		}
+
+		curr = root
 	}
 
 	return curr, nil
+}
+
+func (r *resolver) splitOffNamePart() string {
+	slashPos := strings.Index(r.path, "/")
+
+	var name string
+
+	if slashPos == -1 {
+		name, r.path = r.path, ""
+	} else {
+		name, r.path = r.path[:slashPos], r.path[slashPos+1:]
+		r.cutAt += slashPos + 1
+	}
+
+	return name
+}
+
+func isEmptyName(name string) bool {
+	return name == "" || name == "."
+}
+
+func (r *resolver) handleSymlink(sym *dirEnt) error {
+	r.redirectsRemaining--
+	if r.redirectsRemaining == 0 {
+		return fs.ErrInvalid
+	}
+
+	symPath, err := sym.bytes()
+	if err != nil {
+		return err
+	}
+
+	symPathS := string(symPath)
+	if strings.HasPrefix(symPathS, "/") {
+		r.fullPath = path.Clean(symPathS)[1:]
+	} else if r.path == "" {
+		r.fullPath = path.Join(r.fullPath[:r.cutAt], symPathS)
+	} else {
+		r.fullPath = path.Join(r.fullPath[:r.cutAt-len(sym.name)-1], symPathS, r.path)
+	}
+
+	r.path = r.fullPath
+	r.cutAt = 0
+
+	return nil
 }
 
 func (f *fsRO) getLEntry(path string) (*dirEnt, error) {
